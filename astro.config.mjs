69ac19cc064311e7fa7d5878ce6013accbbb549e
Ade Putra Susila <adeputraas@MacBooks-MacBook-Pro.local>
2026-04-02 15:42:38 +0700

    fix: fix deployment on production vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adeputrasusila.dev',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
